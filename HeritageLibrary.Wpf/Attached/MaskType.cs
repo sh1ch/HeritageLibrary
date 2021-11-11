b7e07b3a4edcb6fc166323b54d0f47945602c8c6
shuichi <isogucchi@outlook.com>
2021-11-11 17:26:49 +0900

    feat: 入力値の制限をする添付プロパティを追加。

---
+++ HeritageLibrary.Wpf/Attached/MaskType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage.Wpf.Attached
+{
+    /// <summary>
+    /// <see cref="MaskType"/> 列挙型は、<see cref="Mask"/> の設定方法を示す識別子を表します。
+    /// </summary>
+    public enum MaskType : int
+    {
+        /// <summary>
+        /// 未設定
+        /// </summary>
+        None = 0x00,
+        /// <summary>
+        /// 半角数字 (0-9)
+        /// </summary>
+        HalfNumeric = 0x01,
+        /// <summary>
+        /// 半角英数字 (a-zA-Z0-9)
+        /// </summary>
+        HalfAlphanumeric = 0x02,
+        /// <summary>
+        /// 全角かな
+        /// </summary>
+        HIRAGANA = 0x03,
+        /// <summary>
+        /// 全角カナ
+        /// </summary>
+        KATAKANA = 0x04,
+    }
+
+}
---
+++ HeritageLibrary.Wpf/Attached/TemplateMask.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Heritage.Wpf.Attached
+{
+    /// <summary>
+    /// <see cref="TemplateMask"/> クラスは、入力値を制限する添付プロパティを定義するクラスです。
+    /// <para>
+    /// <para>
+    /// <see cref="TextBox"/> に使用する添付プロパティです。
+    /// </para>
+    /// </summary>
+    public class TemplateMask
+    {
+        public static readonly DependencyProperty TypeProperty =
+            DependencyProperty.RegisterAttached(
+                "Type",
+                typeof(MaskType),
+                typeof(TemplateMask),
+                new PropertyMetadata(MaskType.None, (sender, args) =>
+                {
+                    if (sender is not TextBox textBox) return;
+
+                    var maskText = (MaskType)args.NewValue switch
+                    {
+                        MaskType.HalfNumeric => "^[0-9]+$",
+                        MaskType.HalfAlphanumeric => "^[a-zA-Z0-9]+$",
+                        MaskType.HIRAGANA => "^[ぁ-ん!ー]+$",
+                        MaskType.KATAKANA => "^[ァ-ヴ!ー]+$",
+                        _ => "",
+                    };
+
+                    // イベントの事前クリア
+                    DataObject.RemovePastingHandler(textBox, Mask_Pasting);
+                    textBox.PreviewTextInput -= Mask_PreviewTextInput;
+                    textBox.PreviewKeyDown -= Mask_PreviewKeyDown;
+
+                    if (maskText != "")
+                    {
+                        SetRegex(textBox, new Regex(maskText, RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace));
+
+                        // イベントを登録
+                        DataObject.AddPastingHandler(textBox, Mask_Pasting);
+                        textBox.PreviewTextInput += Mask_PreviewTextInput;
+                        textBox.PreviewKeyDown += Mask_PreviewKeyDown;
+                    }
+                    else
+                    {
+                        // 使用されていないときは、添付プロパティをクリアする
+                        textBox.ClearValue(RegexProperty);
+                    }
+                }));
+
+        [AttachedPropertyBrowsableForType(typeof(TextBox))]
+        public static MaskType GetType(DependencyObject obj)
+        {
+            return (MaskType)obj.GetValue(TypeProperty);
+        }
+
+        [AttachedPropertyBrowsableForType(typeof(TextBox))]
+        public static void SetType(DependencyObject obj, MaskType value)
+        {
+            obj.SetValue(TypeProperty, value);
+        }
+
+        private static readonly DependencyPropertyKey RegexPropertyKey =
+            DependencyProperty.RegisterAttachedReadOnly(
+                "Regex",
+                typeof(Regex),
+                typeof(TemplateMask),
+                new PropertyMetadata());
+
+        public static readonly DependencyProperty RegexProperty =
+            RegexPropertyKey.DependencyProperty;
+
+        private static Regex GetRegex(DependencyObject obj)
+        {
+            return obj.GetValue(RegexProperty) as Regex;
+        }
+
+        private static void SetRegex(DependencyObject obj, Regex value)
+        {
+            obj.SetValue(RegexPropertyKey, value);
+        }
+
+        private static void Mask_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (sender is not TextBox textBox) return;
+
+            var regex = GetRegex(textBox);
+
+            if (regex == null) return;
+
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                var pastedText = e.DataObject.GetData(typeof(string)) as string;
+                var proposedText = GetProposedText(textBox, pastedText);
+
+                if (regex.IsMatch(proposedText) == false)
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private static void Mask_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (sender is not TextBox textBox) return;
+
+            var regex = GetRegex(textBox);
+
+            if (regex == null) return;
+
+            string input = "";
+
+            // PreviewTextInput が発生しない原因不明の例外キー (例:Space) は、ここで処理します
+            switch (e.Key)
+            {
+                case Key.Space:
+                    input = " ";
+                    break;
+                default:
+                    break;
+            }
+
+            if (input != "")
+            {
+                var proposedText = GetProposedText(textBox, input);
+
+                if (regex.IsMatch(proposedText) == false)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private static void Mask_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            if (sender is not TextBox textBox) return;
+
+            var regex = GetRegex(textBox);
+
+            if (regex == null) return;
+
+            var proposedText = GetProposedText(textBox, e.Text);
+
+            if (regex.IsMatch(proposedText) == false)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private static string GetProposedText(TextBox textBox, string newText)
+        {
+            var text = textBox?.Text ?? "";
+
+            if ((textBox?.SelectionStart ?? -1) != -1)
+            {
+                text = text.Remove(textBox.SelectionStart, textBox.SelectionLength);
+            }
+
+            text = text.Insert(textBox.CaretIndex, newText);
+
+            return text;
+        }
+
+    }
+}
