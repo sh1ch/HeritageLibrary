123933d4d82d5971f4ce18018b2ee6f14eae4720
sh1ch <isogucchi-code@outlook.com>
2022-01-06 16:37:12 +0900

    feat: Arrow およびスタイルを追加

--- HeritageLibrary.Wpf.Controls/Drawings/Arrow1.xaml.cs
+++ HeritageLibrary.Wpf.Controls/Drawings/Arrow1.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HeritageLibrary.Wpf.Controls.Drawings;
+namespace Heritage.Wpf.Controls.Drawings;
 
 /// <summary>
 /// Arrow1.xaml の相互作用ロジック
--- HeritageLibrary.Wpf.Controls/ValueConverters/BoolToScaleXConverter.cs
+++ HeritageLibrary.Wpf.Controls/ValueConverters/BoolToScaleXConverter.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace HeritageLibrary.Wpf.Controls.ValueConverters;
+namespace Heritage.Wpf.Controls.ValueConverters;
 
 internal class BoolToScaleXConverter : IValueConverter
 {
