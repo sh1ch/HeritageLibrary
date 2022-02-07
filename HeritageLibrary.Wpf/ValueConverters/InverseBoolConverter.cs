2d97921db88cff2e5929aaddd75db25de8bcd72c
sh1ch <isogucchi-code@outlook.com>
2022-02-07 16:41:35 +0900

    feat: ValueConverter の追加

---
+++ HeritageLibrary.Wpf/ValueConverters/InverseBoolConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Heritage.Wpf.ValueConverters;
+
+/// <summary>
+/// <see cref="InverseBoolConverter"/> クラスは、カスタム ロジックをバインディングに適用する方法を提供します。 
+/// <para>
+/// <seealso cref="bool?"/> 型のデータを反転した値に変換します。
+/// </para>
+/// </summary>
+[ValueConversion(typeof(bool?), typeof(bool?))]
+public class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+        {
+            return null;
+        }
+
+        if (targetType != typeof(bool) && targetType != typeof(bool?))
+        {
+            return null;
+        }
+
+        return !(bool)value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+        {
+            return null;
+        }
+
+        if (targetType != typeof(bool) && targetType != typeof(bool?))
+        {
+            return null;
+        }
+
+        return !(bool)value;
+    }
+}
---
+++ HeritageLibrary.Wpf/ValueConverters/ResourcesDescriptionConverter.cs
@@ -0,0 +1,45 @@
+﻿using Heritage.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Heritage.Wpf.ValueConverters;
+
+/// <summary>
+/// <see cref="ResourcesDescriptionConverter"/> クラスは、カスタム ロジックをバインディングに適用する方法を提供します。 
+/// <para>
+/// <seealso cref="object"/> 型のデータが持つ属性値を変換値とします。
+/// </para>
+/// </summary>
+[ValueConversion(typeof(object), typeof(string))]
+public class ResourcesDescriptionConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+        {
+            return "";
+        }
+
+        var attributeValue = "";
+        var fieldInfo = value.GetType().GetField(value.ToString());
+
+        if (fieldInfo != null)
+        {
+            var attribute = fieldInfo.GetCustomAttribute(typeof(ResourcesDescriptionAttribute), false) as ResourcesDescriptionAttribute;
+            attributeValue = attribute?.Description ?? "";
+        }
+
+        return attributeValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new InvalidOperationException();
+    }
+}
