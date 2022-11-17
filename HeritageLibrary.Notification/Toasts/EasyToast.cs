c5719300c59a1130da83d9c8a07adc5769871c63
sh1ch <isogucchi-code@outlook.com>
2022-11-17 15:46:42 +0900

    feat: 通知機能のプロジェクトを追加

---
+++ HeritageLibrary.Notification/Toasts/EasyToast.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage.Toasts;
+
+/// <summary>
+/// <see cref="EasyToast"/> クラスは、簡易なトースト通知をサポートします。
+/// </summary>
+public static class EasyToast
+{
+    /// <summary>
+    /// トーストを表示します。
+    /// </summary>
+    /// <param name="title">タイトル。</param>
+    /// <param name="text">テキスト。</param>
+    public static void Show(string title, string text)
+    {
+        new ToastContentBuilder()
+            .AddText(title)
+            .AddText(text)
+            .Show();
+    }
+}
---
+++ HeritageLibrary.Notification/Toasts/ToastIcon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage.Toasts;
+
+/// <summary>
+/// <see cref="ToastIcon"/> 列挙型は、トースト表示する際のアイコン種別を示します。
+/// </summary>
+public enum ToastIcon
+{
+    /// <summary>
+    /// 情報。
+    /// </summary>
+    Info,
+    /// <summary>
+    /// 注意。
+    /// </summary>
+    Warning,
+    /// <summary>
+    /// エラー。
+    /// </summary>
+    Error,
+}
