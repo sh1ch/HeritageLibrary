8a5a96e3dfab45a5d5fd1c0398bbb2aa6cc6b50c
sh1ch <isogucchi-code@outlook.com>
2022-11-28 17:33:36 +0900

    feat: params の一括比較に対応

---
+++ HeritageLibrary.Test/ExtensionTest.cs
@@ -0,0 +1,77 @@
+﻿using Heritage.Linq;
+using Heritage.Systems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Heritage.Test;
+
+public class ExtensionTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCase(100, true, new int[] { -10, 0, 10, 20, 30, 40, 60, 80, 99 })]
+    [TestCase(100, false, new int[] { -10, 0, 10, 20, 30, 40, 60, 80, 99, 100 })]
+    [TestCase(100, false, new int[] { -10, 0, 10, 20, 30, 40, 60, 80, 99, 101 })]
+    public void Test_GreaterThan_Int(int testValue, bool answer, int[] parameters)
+    {
+        var result = testValue.GreaterThan(parameters);
+
+        Assert.AreEqual(result, answer);
+    }
+
+    [TestCase(100, true, new int[] { -10, 0, 10, 20, 30, 40, 60, 80, 99 })]
+    [TestCase(100, true, new int[] { -10, 0, 10, 20, 30, 40, 60, 80, 99, 100 })]
+    [TestCase(100, false, new int[] { -10, 0, 10, 20, 30, 40, 60, 80, 99, 101 })]
+    public void Test_GreaterThanOrEqual_Int(int testValue, bool answer, int[] parameters)
+    {
+        var result = testValue.GreaterThanOrEqual(parameters);
+
+        Assert.AreEqual(result, answer);
+    }
+
+    [TestCase(100, true, new int[] { 101, 120, 130, 140 })]
+    [TestCase(100, false, new int[] { 100, 101, 120, 130, 140 })]
+    [TestCase(100, false, new int[] { 0, 120, 130, 140 })]
+    public void Test_LessThan_Int(int testValue, bool answer, int[] parameters)
+    {
+        var result = testValue.LessThan(parameters);
+
+        Assert.AreEqual(result, answer);
+    }
+
+    [TestCase(100, true, new int[] { 101, 120, 130, 140 })]
+    [TestCase(100, true, new int[] { 100, 101, 120, 130, 140 })]
+    [TestCase(100, false, new int[] { 0, 120, 130, 140 })]
+    public void Test_LessThanOrEqual_Int(int testValue, bool answer, int[] parameters)
+    {
+        var result = testValue.LessThanOrEqual(parameters);
+
+        Assert.AreEqual(result, answer);
+    }
+
+    [TestCase(100D, true, new double[] { -10, 0, 10, 20, 30, 40, 60, 80, 99 })]
+    [TestCase(100D, false, new double[] { -10, 0, 10, 20, 30, 40, 60, 80, 99, 100 })]
+    [TestCase(100D, false, new double[] { -10, 0, 10, 20, 30, 40, 60, 80, 99, 101 })]
+    public void Test_GreaterThan_Double(double testValue, bool answer, double[] parameters)
+    {
+        var result = testValue.GreaterThan(parameters);
+
+        Assert.AreEqual(result, answer);
+    }
+
+    [TestCase(100F, true, new float[] { -10, 0, 10, 20, 30, 40, 60, 80, 99 })]
+    [TestCase(100F, false, new float[] { -10, 0, 10, 20, 30, 40, 60, 80, 99, 100 })]
+    [TestCase(100F, false, new float[] { -10, 0, 10, 20, 30, 40, 60, 80, 99, 101 })]
+    public void Test_GreaterThan_Float(float testValue, bool answer, float[] parameters)
+    {
+        var result = testValue.GreaterThan(parameters);
+
+        Assert.AreEqual(result, answer);
+    }
+}
--- HeritageLibrary/Extension.cs
+++ HeritageLibrary/Extension.cs
@@ -34,12 +34,12 @@ public static class Extension
     }
 
     /// <summary>
-    /// オブジェクトは、 引数のいずれかの値と等しいかどうかを判断します。
+    /// オブジェクトは、引数のいずれかの値と等しいかどうかを判断します。
     /// </summary>
     /// <typeparam name="T">確認をするオブジェクトの型。</typeparam>
     /// <param name="own">値を確認するオブジェクト。</param>
     /// <param name="items">含んでいるかどうかを確認する値。</param>
-    /// <returns>等しい値を含んでいたとき true 。それ以外のとき false 。</returns>
+    /// <returns>等しい値を含んでいたとき <c>true</c>、それ以外のとき <c>false</c>。</returns>
     public static bool IsAny<T>(this T own, params T[] items) where T : IComparable
     {
         bool isEquals = false;
@@ -54,5 +54,113 @@ public static class Extension
         }
 
         return isEquals;
+    }
+
+    /// <summary>
+    /// オブジェクトは、引数のいずれよりも大きい値かどうかを判断します。
+    /// </summary>
+    /// <typeparam name="T">確認をするオブジェクトの型。</typeparam>
+    /// <param name="own">値を確認するオブジェクト。</param>
+    /// <param name="items">含んでいるかどうかを確認する値。</param>
+    /// <returns>条件を満たすとき <c>true</c>、それ以外のとき <c>false</c>。</returns>
+    public static bool GreaterThan<T>(this T own, params T[] items) where T : IComparable
+    {
+        bool isGreater = true;
+
+        foreach (var item in items)
+        {
+            if (own.CompareTo(item) <= 0)
+            {
+                isGreater = false;
+                break;
+            }
+        }
+
+        return isGreater;
+    }
+
+    /// <summary>
+    /// オブジェクトは、引数のいずれよりも大きい値、または、同じ値かどうかを判断します。
+    /// </summary>
+    /// <typeparam name="T">確認をするオブジェクトの型。</typeparam>
+    /// <param name="own">値を確認するオブジェクト。</param>
+    /// <param name="items">含んでいるかどうかを確認する値。</param>
+    /// <returns>条件を満たすとき <c>true</c>、それ以外のとき <c>false</c>。</returns>
+    public static bool GreaterThanOrEqual<T>(this T own, params T[] items) where T : IComparable
+    {
+        bool isGreater = true;
+
+        foreach (var item in items)
+        {
+            if (own.CompareTo(item) < 0)
+            {
+                isGreater = false;
+                break;
+            }
+        }
+
+        return isGreater;
+    }
+
+    /// <summary>
+    /// オブジェクトは、引数のいずれよりも小さい値かどうかを判断します。
+    /// </summary>
+    /// <typeparam name="T">確認をするオブジェクトの型。</typeparam>
+    /// <param name="own">値を確認するオブジェクト。</param>
+    /// <param name="items">含んでいるかどうかを確認する値。</param>
+    /// <returns>条件を満たすとき <c>true</c>、それ以外のとき <c>false</c>。</returns>
+    public static bool LessThan<T>(this T own, params T[] items) where T : IComparable
+    {
+        bool isLess = true;
+
+        foreach (var item in items)
+        {
+            if (own.CompareTo(item) >= 0)
+            {
+                isLess = false;
+                break;
+            }
+        }
+
+        return isLess;
+    }
+
+    /// <summary>
+    /// オブジェクトは、引数のいずれよりも小さい値、または、同じ値かどうかを判断します。
+    /// </summary>
+    /// <typeparam name="T">確認をするオブジェクトの型。</typeparam>
+    /// <param name="own">値を確認するオブジェクト。</param>
+    /// <param name="items">含んでいるかどうかを確認する値。</param>
+    /// <returns>条件を満たすとき <c>true</c>、それ以外のとき <c>false</c>。</returns>
+    public static bool LessThanOrEqual<T>(this T own, params T[] items) where T : IComparable
+    {
+        bool isLess = true;
+
+        foreach (var item in items)
+        {
+            if (own.CompareTo(item) > 0)
+            {
+                isLess = false;
+                break;
+            }
+        }
+
+        return isLess;
+    }
+
+    /// <summary>
+    /// バイト配列のデータを 16 進数のテキストに変換します。
+    /// </summary>
+    /// <param name="bytes">バイト配列のデータ。</param>
+    /// <param name="distance">バイトデータとバイトデータの間に含める区間テキスト。</param>
+    /// <returns>16 進数のテキストデータ。データが存在しないとき空白文字 <c>""</c> を返却します。</returns>
+    /// <exception cref="ArgumentNullException">バイト配列が <c>null</c> のとき発生する例外。</exception>
+    public static string ToHex(this byte[] bytes, string distance = "-")
+    {
+        if (bytes == null) throw new ArgumentNullException(nameof(bytes));
+        if (bytes.Length <= 0) return "";
+
+        var hex = BitConverter.ToString(bytes);
+        return hex.Replace("-", distance);
     }
 }
