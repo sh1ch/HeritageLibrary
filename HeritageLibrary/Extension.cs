ef44f1d4d51bf1dede28572897a635ab72e6be5e
sh1ch <isogucchi-code@outlook.com>
2023-03-03 19:25:03 +0900

    feat: IsAny の機能を追加

--- HeritageLibrary/Extension.cs
+++ HeritageLibrary/Extension.cs
@@ -40,6 +40,52 @@ public static class Extension
     /// <param name="own">値を確認するオブジェクト。</param>
     /// <param name="items">含んでいるかどうかを確認する値。</param>
     /// <returns>等しい値を含んでいたとき <c>true</c>、それ以外のとき <c>false</c>。</returns>
+    public static bool IsAny(this int own, params int[] items)
+    {
+        bool isEquals = false;
+
+        foreach (var item in items) 
+        {
+            if (own.Equals(item))
+            {
+                isEquals = true;
+                break;
+            }
+        }
+
+        return isEquals;
+    }
+
+    /// <summary>
+    /// オブジェクトは、引数のいずれかの値と等しいかどうかを判断します。
+    /// </summary>
+    /// <typeparam name="T">確認をするオブジェクトの型。</typeparam>
+    /// <param name="own">値を確認するオブジェクト。</param>
+    /// <param name="items">含んでいるかどうかを確認する値。</param>
+    /// <returns>等しい値を含んでいたとき <c>true</c>、それ以外のとき <c>false</c>。</returns>
+    public static bool IsAny(this double own, params int[] items)
+    {
+        bool isEquals = false;
+
+        foreach (var item in items)
+        {
+            if (own.Equals(item))
+            {
+                isEquals = true;
+                break;
+            }
+        }
+
+        return isEquals;
+    }
+
+    /// <summary>
+    /// オブジェクトは、引数のいずれかの値と等しいかどうかを判断します。
+    /// </summary>
+    /// <typeparam name="T">確認をするオブジェクトの型。</typeparam>
+    /// <param name="own">値を確認するオブジェクト。</param>
+    /// <param name="items">含んでいるかどうかを確認する値。</param>
+    /// <returns>等しい値を含んでいたとき <c>true</c>、それ以外のとき <c>false</c>。</returns>
     public static bool IsAny<T>(this T own, params T[] items) where T : IComparable
     {
         bool isEquals = false;
