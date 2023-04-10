cca60c783e44da0c6a0912ba679a68c571e173c3
sh1ch <isogucchi-code@outlook.com>
2023-04-10 16:22:54 +0900

    feat: bool 用 value converter を追加

---
+++ HeritageLibrary.Wpf/ValueConverters/NullableBoolToBoolConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Heritage.Wpf.ValueConverters;
+
+/// <summary>
+/// <see cref="NullableBoolToBoolConverter"/> クラスは、 クラスは、カスタム ロジックをバインディングに適用する方法を提供します。 
+/// <para>
+/// <seealso cref="bool?"/> 型のデータを、<seealso cref="bool"/> に変換します。
+/// </para>
+/// </summary>
+[ValueConversion(typeof(bool?), typeof(bool))]
+public class NullableBoolToBoolConverter : DependencyObject, IValueConverter
+{
+    public static readonly DependencyProperty NullValueProperty =
+            DependencyProperty.Register(
+                nameof(NullValue),
+                typeof(bool),
+                typeof(NullableBoolToBoolConverter),
+                new PropertyMetadata(false)
+            );
+
+    public bool NullValue
+    {
+        get => (bool)GetValue(NullValueProperty);
+        set => SetValue(NullValueProperty, value);
+    }
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+        {
+            return NullValue;
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value;
+}
