48d2073e5ddb054e86f70b68bc7c0c33657a9213
sh1ch <isogucchi-code@outlook.com>
2023-12-08 11:53:36 +0900

    feat: TabControl を追加。

---
+++ HeritageLibrary.Wpf.Controls/EarlessTab.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+
+namespace Heritage.Wpf.Control;
+
+public class EarlessTab : TabControl
+{
+    static EarlessTab()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(EarlessTab), new FrameworkPropertyMetadata(typeof(EarlessTab)));
+    }
+}
