164db68d1c7ec3f328de6e79a5dd39a8ba9ca906
sh1ch <isogucchi-code@outlook.com>
2023-12-18 18:44:48 +0900

    feat: FontIcon の追加

---
+++ HeritageLibrary.Wpf.Controls/FontIcon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace Heritage.Wpf.Control;
+
+public class FontIcon : TabControl
+{
+	public static readonly DependencyProperty GlyphProperty = 
+		DependencyProperty.Register(
+			nameof(Glyph), 
+			typeof(char), 
+			typeof(FontIcon), 
+			new PropertyMetadata('\uE7E8'));
+
+	public char Glyph
+	{
+		get => (char)GetValue(GlyphProperty);
+		set => SetValue(GlyphProperty, value);
+	}
+
+	static FontIcon()
+	{
+		DefaultStyleKeyProperty.OverrideMetadata(typeof(FontIcon), new FrameworkPropertyMetadata(typeof(FontIcon)));
+	}
+}
