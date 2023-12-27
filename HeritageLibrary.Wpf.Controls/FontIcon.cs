11e11ce0c3a9604d1af4073ea9b299f7a09c0bac
sh1ch <isogucchi-code@outlook.com>
2023-12-27 17:39:55 +0900

    feat: FontIcon の追加

--- HeritageLibrary.Wpf.Controls/FontIcon.cs
+++ HeritageLibrary.Wpf.Controls/FontIcon.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows;
+using System.Runtime.CompilerServices;
 
 namespace Heritage.Wpf.Control;
 
@@ -21,6 +22,32 @@ public class FontIcon : TabControl
 	{
 		get => (char)GetValue(GlyphProperty);
 		set => SetValue(GlyphProperty, value);
+	}
+
+	public static readonly DependencyProperty GlyphBrushProperty =
+		DependencyProperty.Register(
+			nameof(GlyphBrush),
+			typeof(System.Windows.Media.Brush),
+			typeof(FontIcon),
+			new PropertyMetadata(System.Windows.Media.Brushes.Black));
+
+	public System.Windows.Media.Brush GlyphBrush
+	{
+		get => (System.Windows.Media.Brush)GetValue(GlyphBrushProperty);
+		set => SetValue(GlyphBrushProperty, value);
+	}
+
+	public static readonly DependencyProperty GlyphSizeProperty =
+		DependencyProperty.Register(
+			nameof(GlyphSize),
+			typeof(int),
+			typeof(FontIcon),
+			new PropertyMetadata(16));
+
+	public int GlyphSize
+	{
+		get => (int)GetValue(GlyphSizeProperty);
+		set => SetValue(GlyphSizeProperty, value);
 	}
 
 	static FontIcon()
