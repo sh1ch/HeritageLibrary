cb0c3dd25ad3009d10b52610b17b0b64770e8bc2
sh1ch <isogucchi-code@outlook.com>
2024-07-18 15:03:15 +0900

    feat: NLog 用のプロジェクトを追加

---
+++ HeritageLibrary.NLog/DebugTraceListener.cs
@@ -0,0 +1,34 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeritageLibrary.NLog;
+
+/// <summary>
+/// <see cref="DebugTraceListener"/> クラスは、NLog 用の出力に対応したトレース出力、または、デバッグ出力を監視する最もシンプルなリスナーです。
+/// </summary>
+public class DebugTraceListener : TraceListener
+{
+	private static readonly Logger _Logger = LogManager.GetCurrentClassLogger();
+
+	public override void Write(string? message)
+	{
+		_Logger.Debug(message ?? "");
+	}
+
+	public override void WriteLine(string? message)
+	{
+		_Logger.Debug(message ?? "");
+	}
+
+	/// <summary>
+	/// <see cref="DebugTraceListener"/> クラスの新しいインスタンスを初期化します。
+	/// </summary>
+	public DebugTraceListener()
+	{
+	}
+}
---
+++ HeritageLibrary.NLog/LevelTraceListener.cs
@@ -0,0 +1,64 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeritageLibrary.NLog;
+
+/// <summary>
+/// <see cref="LevelTraceListener"/> クラスは、NLog 用の出力に対応したトレース出力、または、デバッグ出力を監視するリスナーです。
+/// </summary>
+public class LevelTraceListener : TraceListener
+{
+	private static readonly Logger _Logger = LogManager.GetCurrentClassLogger();
+	private Action<string> _Action;
+
+	public LogLevel Level { get; }
+
+	public override void Write(string? message)
+	{
+		_Action.Invoke(message ?? "");
+	}
+
+	public override void WriteLine(string? message)
+	{
+		_Action.Invoke(message ?? "");
+	}
+
+	/// <summary>
+	/// <see cref="LevelTraceListener"/> クラスの新しいインスタンスを初期化します。
+	/// </summary>
+	/// <param name="level">ログの出力レベル。</param>
+	public LevelTraceListener(LogLevel level)
+	{
+		Level = level;
+
+		Action<string> action = level.Name switch
+		{
+			"Trace" => Trace,
+			"Debug" => Debug,
+			"Info" => Info,
+			"Warn" => Warn,
+			"Error" => Error,
+			"Fatal" => Fatal,
+			"Off" => Off,
+			_ => Debug,
+		};
+
+		_Action = action;
+	}
+
+	private void Trace(string message) => _Logger.Trace(message);
+	private void Debug(string message) => _Logger.Debug(message);
+	private void Info(string message) => _Logger.Info(message);
+	private void Warn(string message) => _Logger.Warn(message);
+	private void Error(string message) => _Logger.Error(message);
+	private void Fatal(string message) => _Logger.Fatal(message);
+	private void Off(string message)
+	{
+		// NOP
+	}
+}
