1081600d9aaff496f66c5be599652f92a6b3fa1a
sh1ch <isogucchi-code@outlook.com>
2024-08-23 10:23:20 +0900

    feat: WindowService の追加

---
+++ HeritageLibrary.Wpf/IWindowService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Heritage.Wpf;
+
+/// <summary>
+/// <see cref="IWindowService"/> インターフェースは、<see cref="System.Windows.Window"/> に関するサービスを提供します。
+/// </summary>
+public interface IWindowService
+{
+	Window Window { get; }
+	IntPtr WindowHandle { get; }
+
+	void Close();
+}
---
+++ HeritageLibrary.Wpf/WindowService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Heritage.Wpf;
+
+/// <summary>
+/// <see cref="WindowService"/> クラスは、<see cref="System.Windows.Window"/> に関するサービスを提供します。
+/// </summary>
+public class WindowService : IWindowService
+{
+	private readonly Window _window;
+
+	public Window Window => _window;
+	public IntPtr WindowHandle => new System.Windows.Interop.WindowInteropHelper(_window).Handle;
+
+	/// <summary>
+	/// <see cref="WindowService"/> クラスの新しいインスタンスを初期化します。
+	/// </summary>
+	/// <param name="window">ウィンドウのインスタンス。</param>
+	public WindowService(Window window)
+	{
+		if (window == null)
+		{
+			throw new ArgumentNullException(nameof(window));
+		}
+
+		_window = window;
+
+		// WindowHandle のアドレス値は、Window の非表示前だと 0x00 が返却される。
+		// Window 表示後のボタン押下のタイミングなどで取得されることが望ましい
+	}
+
+	public void Close() => _window.Close();
+}
