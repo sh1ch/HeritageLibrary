0beba8edcddf488fd1fda2028e635d082dc55a22
sh1ch <isogucchi-code@outlook.com>
2024-10-11 14:10:50 +0900

    feat: WindowsHello に対応

---
+++ HeritageLibrary.Windows/Security.Credentials/WindowsHello.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Security.Credentials.UI;
+
+namespace Heritage.Security.Credentials;
+
+/// <summary>
+/// <see cref="WindowsHello"/> クラスは、WindowsHello の機能をサポートします。
+/// </summary>
+public class WindowsHello
+{
+	/// <summary>
+	/// Microsoft Passport PIN、Windows Hello、指紋リーダーなどの認証デバイスが使用できるかどうかを確認します。
+	/// </summary>
+	/// <returns>可用性チェック操作の結果を表す <see cref="UserConsentVerifierAvailability"/> の値。</returns>
+	public static async Task<UserConsentVerifierAvailability> CheckAvailabilityAsync() => await UserConsentVerifier.CheckAvailabilityAsync();
+
+	public static async Task<bool> Request(string message)
+	{
+		var isVerified = false;
+		var ucvAvailability = await UserConsentVerifier.CheckAvailabilityAsync();
+
+		if (ucvAvailability == UserConsentVerifierAvailability.Available)
+		{
+			var consentResult = await UserConsentVerifier.RequestVerificationAsync(message);
+
+			if (consentResult == UserConsentVerificationResult.Verified)
+			{
+				isVerified = true;
+			}
+		}
+
+		return isVerified;
+	}
+}
