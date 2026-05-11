b88031d5718174ec4e1a349bd6ee841a6ae31e55
shuichi <isogucchi@outlook.com>
2026-05-11 16:04:29 +0900

    up

---
+++ HeritageLibrary.Windows/Timers/PeriodicIntervalTimerFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heritage.Timers;
+
+public class PeriodicIntervalTimerFactory : IIntervalTimerFactory
+{
+	public IIntervalTimer Create(uint interval)
+	{
+		return new PeriodicIntervalTimer(interval);
+	}
+}
